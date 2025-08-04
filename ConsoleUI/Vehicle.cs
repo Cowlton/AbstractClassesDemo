db340c796362d3b33355200eb4d5d27480f52f34
Colton Briggs <wolflink013@gmail.com>
2025-08-04 08:31:57 -0700

    Made a Vehicle class

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleUI;
+
+public abstract class Vehicle
+{
+    public string Year = "2022";
+    public string Make = "honda";
+    public string Model = "civic";
+
+
+    public abstract void DriveAbstract();
+    
+    public virtual void DriveVirtual()
+    {
+        Console.WriteLine($"you are driving your {Make} {Model}");
+    }
+
+}
